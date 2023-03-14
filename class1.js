f83c85a72e5fdae934b4daa96f3e15e046a79bff
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2023-03-14 18:29:13 +0100

    message

---
+++ class1.js
@@ -0,0 +1,64 @@
+// variable declaration
+
+// var x = 23;
+
+// var x;
+
+// x = 23;
+
+// var x = 23;
+
+// var y = 24;
+
+// var x, y;
+
+// x = 23;
+
+// y = 24;
+
+// console.log(x + y);
+
+// numbers
+
+// js operators
+// 1) arithmetic operator
+// 2) assignment operator
+// 3) Conditional operator
+// 4) comparism operator
+// 5) logical operator
+// 6) Type operator
+
+// arithmetic opertors
+
+// +,-,*,/,%
+
+// var x, y, z;
+
+// x = 21;
+
+// y = 5;
+
+// z = x % y;
+
+// var num = 23;
+
+// num--;
+
+// var x = "23";
+
+// var x = "hello " + "faith";
+
+// parseint
+// parsefloat
+// Number
+// addition sign
+
+// strings
+
+var name = "Precious";
+var school = "Uniben";
+
+// var sentence = "Hello " + name + " welcome to " + school;
+// var sentence = `Hello ${name} welcome to ${school}`;
+
+console.log(sentence);
---
+++ main.js
@@ -0,0 +1,58 @@
+// js data types
+
+// primitive  and reference data type
+
+// primitive => one that is not an object and has no method or properties.
+// Their values are assigned directly in memory
+// e.g are:
+// numbers, strings, booleans, null and undefined
+
+// reference data types => These data types are used to hold other data types
+// e.g are:
+// arrays & objects.
+
+// variable
+
+// var, let and const
+
+// var name = "Precious";
+
+// var age = 20;
+
+// var isMarried = true;
+
+// var isAdmin = false;
+
+// var score = null;
+
+// var level;
+
+// var Mordecai = "true";
+// var age = "23";
+// var myName = "hello i am a boy";
+// var sentence = "mary is a girl";
+// var sentence2 = "Enoch's mother is a good woman";
+// var sentence3 = 'Enoch"s mother is a good woman';
+// var sentence4 = 'GB\'s mother, screamed "whaoooo" when she saw the snake';
+
+// var can be redeclared and reassigned
+
+// var name = "godbless";
+// var name = "godbless";
+// var name = "pheight";
+// name = "pheight";
+// it is globally scoped
+
+// let can be reassigned but cannot be redeclared
+// let name = "godbless";
+// name = "enoch";
+
+// const
+// const name = "godbless";
+
+// name = "pheight";
+
+// output javascript
+// alert
+// console
+// document.write
